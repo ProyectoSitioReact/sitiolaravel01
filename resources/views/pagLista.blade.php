50c8cfdffb2b2fc0960c7cbb16b0454b5467ab8e
Daytiana Ramirez Huayhua <daytiana.ramirez.huayhua@gmail.com>
2022-12-05 11:39:46 -0500

    Subiendo Proyecto Laravel Con Base de Dtaos

--- resources/views/pagLista.blade.php
+++ resources/views/pagLista.blade.php
@@ -9,6 +9,46 @@
     <h3>Lista</h3>
     @foreach($xAlumnos as $item)
         <p> {{ $item->id }} {{ $item->nomEst }} </p>
+        <p> {{ $item->id }} {{ $item->apeEst}} </p>
+        <p> {{ $item->id }} {{ $item->fnaEst}} </p>
+        <p> {{ $item->id }} {{ $item->turmat}} </p>
+        <p> {{ $item->id }} {{ $item->semMat}} </p>
+        <p> {{ $item->id }} {{ $item->estMat}} </p>
+
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Apellido</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Turno</th>
+                    <th scope="col">Semestre</th>
+                    <th scope="col">Estado</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td colspan="2">Larry the Bird</td>
+                <td>@twitter</td>
+            </tr>
+        </tbody>
+        </table>
+
+        
     @endforeach
 
 @endsection
